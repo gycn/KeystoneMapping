31b7b8a7f2137a66d9c40828e02096e978147dbc
Calvin Dong <calvind777@gmail.com>
2016-11-18 23:39:15 -0800

    stuff

--- js/main_script.js
+++ js/main_script.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
   setupMap();
+  $(".features").click(function (e) {
+  		e.stopPropagation();
+        if (document.getElementById("mySidenav").style.width == "250px"){
+        	document.getElementById("mySidenav").style.width = "0";
+        }
+        else {
+        	document.getElementById("mySidenav").style.width = "250px";
+        }
+    });
 });
