12b2c1af7b773c1dbcb926e629ece8bfac197d74
Calvin Dong <calvind777@gmail.com>
2016-11-18 19:12:58 -0800

    make nvd3 work

--- data.js
+++ data.js
@@ -1,40 +1,48 @@
-function myData() {
-    var series1 = [];
-    for(var i =1; i < 100; i ++) {
-        series1.push({
-            x: i, y: 100 / i
-        });
-    }
-
-    return [
-        {
-            key: "Series #1",
-            values: series1,
-            color: "#0000ff"
-        }
-    ];
-}
+var data = [
+      {
+        "label": "One",
+        "value" : 29.765957771107
+      } ,
+      {
+        "label": "Two",
+        "value" : 0
+      } ,
+      {
+        "label": "Three",
+        "value" : 32.807804682612
+      } ,
+      {
+        "label": "Four",
+        "value" : 196.45946739256
+      } ,
+      {
+        "label": "Five",
+        "value" : 0.19434030906893
+      } ,
+      {
+        "label": "Six",
+        "value" : 98.079782601442
+      } ,
+      {
+        "label": "Seven",
+        "value" : 13.925743130903
+      } ,
+      {
+        "label": "Eight",
+        "value" : 5.1387322875705
+      }
+];
 
 nv.addGraph(function() {
-    var chart = nv.models.lineChart();
+  var chart = nv.models.pieChart()
+      .x(function(d) { return d.label })
+      .y(function(d) { return d.value })
+      .showLabels(true);
 
-    chart.xAxis
-        .axisLabel("X-axis Label");
+    d3.select("#chart svg")
+        .datum(data)
+      .transition().duration(1200)
+        .call(chart);
 
-    chart.yAxis
-        .axisLabel("Y-axis Label")
-        .tickFormat(d3.format("d"))
-        ;
-
-    d3.select("svg")
-        .datum(myData())
-        .transition().duration(500).call(chart);
-
-    nv.utils.windowResize(
-            function() {
-                
-            }
-        );
-
-    return chart;
+  return chart;
 });
